e01bfa1e47d025dfcfbcb28be54cc9bda6ec2baa
mikey1977 <mmi@umich.edu>
2015-09-10 16:49:50 -1000

    tests up to DamageSpell passed

--- constructors.js
+++ constructors.js
@@ -11,15 +11,28 @@
  * @method   printDetails
  */
 
+function Spell(name, cost, description) {
+  this.name = name;
+  this.cost = cost;
+  this.description = description;
+}
+
+
+
+
   /**
    * @method printDetails
-   * 
+   *
    * Print out all spell details and format it nicely.
    * The format doesnt matter, as long as it contains the spell name, cost, and description.
    *
    * note: using comma separated arguments for console.log() will not satisfy the tests
    * e.g. console.log(a, b, c); <-- no commas, please use string concatenation.
    */
+
+Spell.prototype.printDetails = function() {
+  console.log("spell name is " + this.name + " spell cost is " + this.cost + " spell does " + this.description);
+};
 
 /**
  * A spell that deals damage.
@@ -46,6 +59,14 @@
  * @property {string} description
  */
 
+function DamageSpell(name, cost, damage, description){
+  Spell.call(this, name, cost, description);
+
+  this.damage = damage;
+}
+DamageSpell.prototype = Object.create(Spell.prototype);
+
+
 /**
  * Now that you've created some spells, let's create
  * `Spellcaster` objects that can use them!
@@ -63,9 +84,19 @@
  * @method  invoke
  */
 
+function Spellcaster(name, health, mana) {
+  this.name = name;
+  this.health = health;
+  this.mana = mana;
+  this.isAlive = true;
+}
+Spellcaster.prototype.inflictDamage(damage){
+
+}
+
   /**
    * @method inflictDamage
-   * 
+   *
    * The spellcaster loses health equal to `damage`.
    * Health should never be negative.
    * If the spellcaster's health drops to 0,
@@ -76,7 +107,7 @@
 
   /**
    * @method spendMana
-   * 
+   *
    * Reduces the spellcaster's mana by `cost`.
    * Mana should only be reduced only if there is enough mana to spend.
    *
@@ -86,7 +117,7 @@
 
   /**
    * @method invoke
-   * 
+   *
    * Allows the spellcaster to cast spells.
    * The first parameter should either be a `Spell` or `DamageSpell`.
    * If it is a `DamageSpell`, the second parameter should be a `Spellcaster`.
